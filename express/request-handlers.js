5ae4e4ce789e306cbf59089889ba87dc7c7dc606
blessykbabu <blessykbabu5@gmail.com>
2023-11-20 15:03:37 +0530

    blog

--- express/request-handlers.js
+++ express/request-handlers.js
@@ -1,6 +1,13 @@
+
+
+
 import bcrypt from "bcrypt";
 import userSchema from "./schema/user.schema.js";
+import jwt from "jsonwebtoken";
 
+
+
+const { sign } = jwt;
 
 export async function register(req, res) {
     try {
@@ -15,6 +22,38 @@ export async function register(req, res) {
         }
         let result = await userSchema.create({ username, password: hashedPass ,email});
         return res.status(200).send("Registration successful!");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error");
+    }
+}
+
+export async function login(req, res) {
+    try {
+        let { username, password } = req.body;
+        if( username.length < 4 && password.length < 4) {
+            return res.json("Invalid username or password");
+        }
+        let user = await userSchema.findOne({ username });
+        if(!user) {
+            return res.status(403).send("Invalid username or password");
+        }
+        let passCheck = await bcrypt.compare(password, user.password);
+        if(passCheck) {
+            let token = await sign({
+                username: user.username,
+                id: user._id
+            },
+            process.env.SECRET_KEY,
+            {
+                expiresIn: "24h"
+            })
+            return res.status(200).json({
+                msg: "Login successful...",
+                token: token
+            })
+        }
+        return res.status(403).send("invalid username or password")
     } catch (error) {
         console.log(error);
         res.status(500).send("Error");
--- express/router.js
+++ express/router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-
+import auth from "./middlewares/auth.js"
 
 import * as rh from "./request-handlers.js";
 
@@ -7,7 +7,7 @@ import * as rh from "./request-handlers.js";
 const router = Router()
 
 router.route("/register").post(rh.register);
-
+router.route("/login").post(rh.login);
 
 
 
